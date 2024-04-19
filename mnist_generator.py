6527466b8545b103278e4566a6ea7515fc46f7fc
Aaron CHan <achn357@gmail.com>
2024-04-19 01:28:32 -0500

    Current code

--- mnist_generator.py
+++ mnist_generator.py
@@ -6,18 +6,27 @@ import scipy.io
 mnist = load_digits()
 
 data_set = scipy.io.loadmat('MNIST_TrainSet_0to1_8x8pixel.mat')     #MNIST dataset in 8x8 resolution
-data_set = data_set['number']
+data = data_set['number']
 label_set = scipy.io.loadmat('MNIST_TrainSet_Label.mat')
 label = label_set['label']
-data_set = data_set.reshape(60000, 64)
-label = label.reshape(60000, 1)
-x = pd.DataFrame(data_set)
-y = pd.DataFrame(label)
+data = data.T
+label = label.T
+x1 = pd.DataFrame(data)
+y1 = pd.DataFrame(label)
 
+# x = pd.DataFrame(mnist.data)
+# y = pd.DataFrame(mnist.target)
+
+# import matplotlib.pyplot as plt
+# image0 = x1.iloc[0].to_numpy().reshape(8,8)
+# breakpoint()
+# plt.imshow(image0)
+# plt.show()
+# breakpoint()
 #x = x / 255.0
 
 train_size = 0.7
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=train_size)
+x_train, x_test, y_train, y_test = train_test_split(x1, y1, train_size=train_size)
 # breakpoint()
 #breakpoint()
 x_train.to_csv("mnist_8x8/train_X.csv", header=False, index=False)
